910b0cadc3512cb6f85e4c54a51c2588c18e697a
Johannes Geppert <post@jgeppert.com>
2010-07-23 12:42:52 +0000

    update combobox plugin

--- struts2-jquery-plugin/src/main/resources/template/js/plugins/jquery.combobox.js
+++ struts2-jquery-plugin/src/main/resources/template/js/plugins/jquery.combobox.js
@@ -1,37 +1,40 @@
 // Taken from jquery UI Combobox Example
 ( function($) {
 	$.widget("ui.combobox", {
-		_create : function() {
+		_create: function() {
 			var self = this;
 			var select = this.element.hide();
-			$("<input>").insertAfter(select).autocomplete( {
-			source : function(request, response) {
-				var matcher = new RegExp(request.term, "i");
-				response(select.children("option").map( function() {
-					var text = $(this).text();
-					if (!request.term || matcher.test(text)) { return {
-					id :$(this).val(),
-					label :text.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + request.term.replace(/([\^\$\(\)\[\]\{\}\*\.\+\?\|\\])/gi, "\\$1") + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>"),
-					value :text
-					}; }
-				}));
-			},
-			delay :0,
-			select : function(e, ui) {
-				if (!ui.item) {
-					// remove invalid value, as it didn't match anything
-					$(this).val("");
-					return false;
-				}
-				$(this).focus();
-				select.val(ui.item.id);
-				self._trigger("selected", null, {
-					item :select.find("[value='" + ui.item.id + "']")
-				});
-
-			},
-			minLength :0
-			}).addClass("ui-widget ui-widget-content ui-corner-left");
+			var input = $("<input>")
+				.insertAfter(select)
+				.autocomplete({
+					source: function(request, response) {
+						var matcher = new RegExp(request.term, "i");
+						response(select.children("option").map(function() {
+							var text = $(this).text();
+							if (this.value && (!request.term || matcher.test(text)))
+								return {
+									id: this.value,
+									label: text.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + $.ui.autocomplete.escapeRegex(request.term) + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>"),
+									value: text
+								};
+						}));
+					},
+					delay: 0,
+					change: function(event, ui) {
+						if (!ui.item) {
+							// remove invalid value, as it didn't match anything
+							$(this).val("");
+							return false;
+						}
+						select.val(ui.item.id);
+						self._trigger("selected", event, {
+							item: select.find("[value='" + ui.item.id + "']")
+						});
+						
+					},
+					minLength: 0
+				})
+				.addClass("ui-widget ui-widget-content ui-corner-left");
 		}
 	});
 })(jQuery);
