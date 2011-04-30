3268728f6e8a2118a41da24399f3528372786310
Johannes Geppert <post@jgeppert.com>
2011-04-30 14:27:54 +0000

    Upgrade to ckeditor 3.5.3

--- struts2-jquery-richtext-plugin/src/main/resources/template/js/ckeditor/config.js
+++ struts2-jquery-richtext-plugin/src/main/resources/template/js/ckeditor/config.js
@@ -8,5 +8,4 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.extraPlugins='onchange';
 };
