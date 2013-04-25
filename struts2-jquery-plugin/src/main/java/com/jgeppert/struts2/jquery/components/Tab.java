1ac946e4211787da03cd9484fb83c88751bb5d8d
Johannes Geppert <post@jgeppert.com>
2013-04-25 19:15:53 +0000

    Solve Issue 964:	Struts 2.3.12 compatibility

--- struts2-jquery-plugin/src/main/java/com/jgeppert/struts2/jquery/components/Tab.java
+++ struts2-jquery-plugin/src/main/java/com/jgeppert/struts2/jquery/components/Tab.java
@@ -28,90 +28,94 @@ import org.apache.struts2.views.annotations.StrutsTagSkipInheritance;
 
 import com.opensymphony.xwork2.util.ValueStack;
 
+import java.util.Random;
+
 /**
  * <!-- START SNIPPET: javadoc -->
  * <p>
  * Renders a local or an remote tab for tabbedPanel
  * </p>
  * <!-- END SNIPPET: javadoc -->
- * 
+ * <p/>
  * <p>
  * Examples
  * </p>
  * <!-- START SNIPPET: example1 -->
- * 
+ * <p/>
  * <pre>
- * &lt;s:url id=&quot;remoteurl1&quot; action=&quot;myremoteactionone&quot;/&gt; 
- * &lt;sj:tab id=&quot;tab1&quot; target=&quot;tone&quot; label=&quot;My Local Tab for div tone&quot;/&gt; 
- * &lt;sj:tab id=&quot;tab2&quot; href=&quot;%{remoteurl1}&quot; label=&quot;My Remote Tab&quot;/&gt; 
+ * &lt;s:url id=&quot;remoteurl1&quot; action=&quot;myremoteactionone&quot;/&gt;
+ * &lt;sj:tab id=&quot;tab1&quot; target=&quot;tone&quot; label=&quot;My Local Tab for div tone&quot;/&gt;
+ * &lt;sj:tab id=&quot;tab2&quot; href=&quot;%{remoteurl1}&quot; label=&quot;My Remote Tab&quot;/&gt;
  * &lt;div id=&quot;tone&quot;&gt; Test 1 &lt;/div&gt;
  * </pre>
- * 
+ * <p/>
  * <!-- END SNIPPET: example1 -->
- * 
+ *
  * @author <a href="http://www.jgeppert.com">Johannes Geppert</a>
- * 
  */
 @StrutsTag(name = "tab", tldTagClass = "com.jgeppert.struts2.jquery.views.jsp.ui.TabTag", description = "Render Tab for Tabbed Pannel providing content from remote call via AJAX")
 public class Tab extends AbstractRemoteBean {
 
-  public static final String TEMPLATE       = "tab";
-  public static final String TEMPLATE_CLOSE = "tab-close";
-  public static final String COMPONENT_NAME = Tab.class.getName();
+    public static final String TEMPLATE = "tab";
+    public static final String TEMPLATE_CLOSE = "tab-close";
+    public static final String COMPONENT_NAME = Tab.class.getName();
+    private final static transient Random RANDOM = new Random();
 
-  protected String           target;
-  protected String           closable;
+    protected String target;
+    protected String closable;
 
-  public Tab(ValueStack stack, HttpServletRequest request, HttpServletResponse response) {
-    super(stack, request, response);
-  }
-
-  public String getDefaultOpenTemplate()
-  {
-    return TEMPLATE;
-  }
-
-  protected String getDefaultTemplate()
-  {
-    return TEMPLATE_CLOSE;
-  }
-
-  public void evaluateExtraParams()
-  {
-    super.evaluateExtraParams();
-    TabbedPanel parentTabbedPanel = (TabbedPanel) findAncestor(TabbedPanel.class);
-    if (parentTabbedPanel != null)
-    {
-      addParameter("parentTabbedPanel", findString(parentTabbedPanel.getId()));
+    public Tab(ValueStack stack, HttpServletRequest request, HttpServletResponse response) {
+        super(stack, request, response);
     }
 
-    if (target != null) addParameter("target", findString(target));
-    if (closable != null) addParameter("closable", findValue(closable, Boolean.class));
-  }
+    public String getDefaultOpenTemplate() {
+        return TEMPLATE;
+    }
 
-  @Override
-  @StrutsTagSkipInheritance
-  public void setTheme(String theme)
-  {
-    super.setTheme(theme);
-  }
+    protected String getDefaultTemplate() {
+        return TEMPLATE_CLOSE;
+    }
 
-  @Override
-  public String getTheme()
-  {
-    return "jquery";
-  }
+    public void evaluateExtraParams() {
+        super.evaluateExtraParams();
+        TabbedPanel parentTabbedPanel = (TabbedPanel) findAncestor(TabbedPanel.class);
+        if (parentTabbedPanel != null) {
+            addParameter("parentTabbedPanel", findString(parentTabbedPanel.getId()));
+        }
 
-  @StrutsTagAttribute(description = "id of target div for tab")
-  public void setTarget(String target)
-  {
-    this.target = target;
-  }
+        if (target != null) addParameter("target", findString(target));
+        if (closable != null) addParameter("closable", findValue(closable, Boolean.class));
 
-  @StrutsTagAttribute(description = "If true then tab is closable.", defaultValue = "false", type = "Boolean")
-  public void setClosable(String closable)
-  {
-    this.closable = closable;
-  }
+        if ((this.id == null || this.id.length() == 0)) {
+            // resolves Math.abs(Integer.MIN_VALUE) issue reported by FindBugs
+            // http://findbugs.sourceforge.net/bugDescriptions.html#RV_ABSOLUTE_VALUE_OF_RANDOM_INT
+            int nextInt = RANDOM.nextInt();
+            nextInt = nextInt == Integer.MIN_VALUE ? Integer.MAX_VALUE : Math
+                    .abs(nextInt);
+            this.id = "tab_" + String.valueOf(nextInt);
+            addParameter("id", this.id);
+        }
+    }
+
+    @Override
+    @StrutsTagSkipInheritance
+    public void setTheme(String theme) {
+        super.setTheme(theme);
+    }
+
+    @Override
+    public String getTheme() {
+        return "jquery";
+    }
+
+    @StrutsTagAttribute(description = "id of target div for tab")
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    @StrutsTagAttribute(description = "If true then tab is closable.", defaultValue = "false", type = "Boolean")
+    public void setClosable(String closable) {
+        this.closable = closable;
+    }
 
 }
