88e690d11fa357e964663d3a0e63b4912642a152
Johannes Geppert <post@jgeppert.com>
2013-05-29 04:43:56 +0000

    Upgrade to CKEditor 4.1.1

--- struts2-jquery-richtext-plugin/src/main/resources/template/js/ckeditor/config.js
+++ struts2-jquery-richtext-plugin/src/main/resources/template/js/ckeditor/config.js
@@ -7,6 +7,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-		config.plugins='dialogui,dialog,about,a11yhelp,basicstyles,blockquote,clipboard,panel,floatpanel,menu,contextmenu,resize,button,toolbar,elementspath,list,indent,enterkey,entities,popup,filebrowser,floatingspace,listblock,richcombo,format,htmlwriter,horizontalrule,wysiwygarea,image,fakeobjects,link,magicline,maximize,pastetext,pastefromword,removeformat,sourcearea,specialchar,menubutton,scayt,stylescombo,tab,table,tabletools,undo,wsc';
+	config.plugins='dialogui,dialog,about,a11yhelp,basicstyles,blockquote,clipboard,panel,floatpanel,menu,contextmenu,resize,button,toolbar,elementspath,list,indent,enterkey,entities,popup,filebrowser,floatingspace,listblock,richcombo,format,htmlwriter,horizontalrule,wysiwygarea,image,fakeobjects,link,magicline,maximize,pastetext,pastefromword,removeformat,sourcearea,specialchar,menubutton,scayt,stylescombo,tab,table,tabletools,undo,wsc';
     config.extraPlugins='onchange';
 };
--- struts2-jquery-showcase/src/main/webapp/js/ckeditor.config.js
+++ struts2-jquery-showcase/src/main/webapp/js/ckeditor.config.js
@@ -1,20 +1,20 @@
-﻿CKEDITOR.editorConfig = function( config )
-{
-	config.plugins='dialogui,dialog,about,a11yhelp,basicstyles,blockquote,clipboard,panel,floatpanel,menu,contextmenu,resize,button,toolbar,elementspath,list,indent,enterkey,entities,popup,filebrowser,floatingspace,listblock,richcombo,format,htmlwriter,horizontalrule,wysiwygarea,image,fakeobjects,link,magicline,maximize,pastetext,pastefromword,removeformat,sourcearea,specialchar,menubutton,scayt,stylescombo,tab,table,tabletools,undo,wsc';
-  config.extraPlugins='onchange';
+﻿CKEDITOR.editorConfig = function (config) {
 
-	config.toolbar = 'MyToolbar';
+    config.plugins = 'dialogui,dialog,about,a11yhelp,basicstyles,blockquote,clipboard,panel,floatpanel,menu,contextmenu,resize,button,toolbar,elementspath,list,indent,enterkey,entities,popup,filebrowser,floatingspace,listblock,richcombo,format,htmlwriter,horizontalrule,wysiwygarea,image,fakeobjects,link,magicline,maximize,pastetext,pastefromword,removeformat,sourcearea,specialchar,menubutton,scayt,stylescombo,tab,table,tabletools,undo,wsc';
+    config.extraPlugins = 'onchange';
 
-  config.toolbar_MyToolbar =
-  [
-      ['NewPage','Preview'],
-      ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Scayt'],
-      ['Undo','Redo','-','Find','Replace','-','Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak''],
-      '/',
-      ['Styles','Format'],
-      ['Bold','Italic','Strike'],
-      ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-      ['Link','Unlink','Anchor']
-  ];
+    config.toolbar = 'MyToolbar';
+
+    config.toolbar_MyToolbar =
+        [
+            ['NewPage', 'Preview'],
+            ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Scayt'],
+            ['Undo', 'Redo', '-', 'Find', 'Replace', '-', 'Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak'],
+            '/',
+            ['Styles', 'Format'],
+            ['Bold', 'Italic', 'Strike'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'],
+            ['Link', 'Unlink', 'Anchor']
+        ];
 
 };
