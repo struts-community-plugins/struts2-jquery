862622b93c61896aab7d15e604dfb3ea2528861a
Stefaan Dutry <stefaan.dutry@gmail.com>
2016-12-24 17:37:32 +0100

    added test for ajax link with multiple targets

--- struts2-jquery-plugin-tests/src/test/java/com/jgeppert/struts2/jquery/a/ATagIT.java
+++ struts2-jquery-plugin-tests/src/test/java/com/jgeppert/struts2/jquery/a/ATagIT.java
@@ -22,15 +22,15 @@ public class ATagIT {
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
                  { "http://localhost:8080/regular" }, 
-	         { "http://localhost:8080/uncompressed" },
-	         { "http://localhost:8080/loadatonce" }, 
-	         { "http://localhost:8080/loadfromgoogle" }  
+                 { "http://localhost:8080/uncompressed" },
+                 { "http://localhost:8080/loadatonce" }, 
+                 { "http://localhost:8080/loadfromgoogle" }  
            });
     }
     
     private static JQueryIdleCondition jQueryIdle = new JQueryIdleCondition();
 
-    private String baseUrl;	
+    private String baseUrl;        
 
     public ATagIT(final String baseUrl) {
         this.baseUrl = baseUrl;
@@ -42,12 +42,36 @@ public class ATagIT {
         WebDriverWait wait = new WebDriverWait(driver, 30);
 
         driver.get(baseUrl + "/a/simple-ajax-link.action");
-        Assert.assertEquals("Click on the link bellow.", driver.findElement(By.id("result")).getText());
-        driver.findElement(By.id("ajaxlink")).click();
+        WebElement resultDiv = driver.findElement(By.id("result"));
+        WebElement ajaxlink = driver.findElement(By.id("ajaxlink"));
+	
+        Assert.assertEquals("Click on the link bellow.", resultDiv.getText());
+        ajaxlink.click();
 
         wait.until(jQueryIdle);
 
-        Assert.assertEquals("This is simple text from an ajax call.", driver.findElement(By.id("result")).getText());
+        Assert.assertEquals("This is simple text from an ajax call.", resultDiv.getText());
+    }
+    
+    @Test
+    public void testMultipleTargets() {
+        WebDriver driver = new HtmlUnitDriver(true);
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+
+        driver.get(baseUrl + "/a/multiple-targets.action");
+        WebElement div1 = driver.findElement(By.id("div1"));
+        WebElement div2 = driver.findElement(By.id("div2"));
+        WebElement ajaxLink = driver.findElement(By.id("ajaxlink"));
+
+        Assert.assertEquals("Div 1", div1.getText());
+        Assert.assertEquals("Div 2", div2.getText());
+
+        ajaxLink.click();
+
+        wait.until(jQueryIdle);
+
+        Assert.assertEquals("This is simple text from an ajax call.", div1.getText());
+        Assert.assertEquals("This is simple text from an ajax call.", div2.getText());
     }
 }
 
