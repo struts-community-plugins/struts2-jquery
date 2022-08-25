be71c1678d4e68a9073ae2673cc5b1faf4cb614e
Johannes Geppert <post@jgeppert.com>
2022-08-25 10:16:53 +0200

    Set default cache factories for ognl utils in integration tests

--- struts2-jquery-plugin/src/test/java/com/jgeppert/struts2/jquery/components/AbstractComponentBaseTest.java
+++ struts2-jquery-plugin/src/test/java/com/jgeppert/struts2/jquery/components/AbstractComponentBaseTest.java
@@ -1,14 +1,15 @@
 package com.jgeppert.struts2.jquery.components;
 
-import org.apache.struts2.conversion.StrutsTypeConverterHolder;
-import org.junit.jupiter.api.BeforeEach;
-
 import com.opensymphony.xwork2.DefaultTextProvider;
 import com.opensymphony.xwork2.conversion.impl.XWorkConverter;
+import com.opensymphony.xwork2.ognl.DefaultOgnlBeanInfoCacheFactory;
+import com.opensymphony.xwork2.ognl.DefaultOgnlExpressionCacheFactory;
 import com.opensymphony.xwork2.ognl.OgnlUtil;
 import com.opensymphony.xwork2.ognl.OgnlValueStack;
 import com.opensymphony.xwork2.ognl.accessor.CompoundRootAccessor;
 import com.opensymphony.xwork2.util.ValueStack;
+import org.apache.struts2.conversion.StrutsTypeConverterHolder;
+import org.junit.jupiter.api.BeforeEach;
 
 public abstract class AbstractComponentBaseTest {
     protected ValueStack valueStack;
@@ -27,7 +28,7 @@ public abstract class AbstractComponentBaseTest {
             converter.setTypeConverterHolder(new StrutsTypeConverterHolder());
 
             setXWorkConverter(converter);
-            setOgnlUtil(new OgnlUtil(null, null));
+            setOgnlUtil(new OgnlUtil(new DefaultOgnlExpressionCacheFactory<>(), new DefaultOgnlBeanInfoCacheFactory<>()));
         }
 
     }
