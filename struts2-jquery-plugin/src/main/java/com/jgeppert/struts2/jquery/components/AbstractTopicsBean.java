53781450b3c7c8cfc222937df68a7d586caca030
Stefaan Dutry <stefaan.dutry@gmail.com>
2022-09-13 17:12:34 +0200

    issue #308 make id field dynamic again

--- struts2-jquery-plugin/src/main/java/com/jgeppert/struts2/jquery/components/AbstractTopicsBean.java
+++ struts2-jquery-plugin/src/main/java/com/jgeppert/struts2/jquery/components/AbstractTopicsBean.java
@@ -118,8 +118,10 @@ public abstract class AbstractTopicsBean extends ClosingUIBean {
             int nextInt = RANDOM.nextInt();
             nextInt = nextInt == Integer.MIN_VALUE ? Integer.MAX_VALUE : Math.abs(nextInt);
             this.id = prefix + String.valueOf(nextInt);
+            addParameter(PARAM_ID, this.id);
+        } else {
+            addParameter(PARAM_ID, findString(this.id));
         }
-        addParameter(PARAM_ID, this.id);
     }
 
     @StrutsTagAttribute(description = "Topics that are published before a load")
